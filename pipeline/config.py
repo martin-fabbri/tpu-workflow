76dadfc90c389af205b4caebda529137212bea78
Martin Fabbri <martin.fabbri.a@gmail.com>
2021-02-21 07:23:49 +0000

    pipeline: train task

--- pipeline/config.py
+++ pipeline/config.py
@@ -6,7 +6,7 @@ from pathlib import Path
 class Config:
     RANDOM_SEED = 47
     ARTIFACTS_PATH = Path("./artifacts").absolute()
-    DATASET_PATH = Path("/home/shared/dvc-external/tpu-workflow/data").absolute()
+    DATASET_PATH = Path("./data").absolute()
     TRAIN_DATA_DIR = str(DATASET_PATH / "train")
     CATS_TRAIN_DATA_DIR = str(DATASET_PATH / "train" / "cats")
     VALIDATION_DATA_DIR = str(DATASET_PATH / "validation")
